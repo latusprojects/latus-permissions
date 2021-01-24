9b4ef9e6d16697c97e331a9a4c7a0005cbbfc7bf
Max Latussek <latusschnecke@gmail.com>
2021-01-24 15:57:43 +0100

    Initial commit

---
+++ src/SteelPermissionsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Steelvirus\LaravelPermissions;
+
+use Illuminate\Support\ServiceProvider;
+
+class SteelPermissionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
