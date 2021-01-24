2b36d750d189891b8424b6ef75f79d1ea818af53
Max Latussek <latusschnecke@gmail.com>
2021-01-24 18:04:09 +0100

    Add 'Permissible' trait Add loading of migrations to service-provider boot

---
+++ src/Models/Traits/Permissible.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Steelvirus\LaravelPermissions\Models\Traits;
+
+use Steelvirus\LaravelPermissions\Models\Permission;
+use Steelvirus\LaravelPermissions\Models\Role;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+trait Permissible
+{
+    public function hasOnePermission(array $names): bool
+    {
+        $permissions = Permission::whereIn('name', $names)->get();
+        if (!$permissions) {
+            return false;
+        }
+
+        foreach ($permissions as $permission) {
+            if ($this->effectivePermissions()->contains($permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function hasPermission(string $name): bool
+    {
+        $permission = Permission::where('name', strtolower($name))->first();
+        if (!$permission) {
+            return false;
+        }
+        return $this->effectivePermissions()->contains($permission);
+    }
+
+    public function effectivePermissions(): Collection
+    {
+        $role_permissions = $this->roles()->with('permissions')->get()->pluck('permissions')->unique()->flatten();
+        $user_permissions = $this->permissions()->get();
+
+        return $user_permissions->concat($role_permissions);
+    }
+
+    public function primaryRole(): Role
+    {
+        return $this->roles()->orderBy('level', 'desc')->first();
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class)->withTimestamps();
+    }
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class)->withTimestamps();
+    }
+}
