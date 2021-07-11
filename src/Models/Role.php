b2bf3f5222ef939959bce52713da1564fd3a5551
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-11 17:47:30 +0200

    Create ResolvesPermissions.php trait, move Role::resolvePermissions method to trait

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Models\User;
+use Latus\Permissions\Models\Traits\ResolvesPermissions;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, ResolvesPermissions;
 
     public function users(): BelongsToMany
     {
@@ -20,24 +21,6 @@ class Role extends Model
     public function effectivePermissions(): Collection
     {
         return $this->permissions()->get();
-    }
-
-    public function resolvePermissions(): \Illuminate\Support\Collection
-    {
-        $simple_permissions = $this->permissions()->where('name', 'not like', '%.*')->get()->pluck('guard', 'name')->all();
-
-        $wildcard_permissions = $this->permissions()->where('name', 'like', '%.*')->get()->pluck('guard', 'name')->all();
-
-        foreach ($wildcard_permissions as $wildcard_permission => $wildcard_permission_guard) {
-            $simple_permissions += Permission::where('name', 'like', substr($wildcard_permission, 0, -1) . '%')
-                ->where('name', 'not like', '%.*')
-                ->whereNotIn('name', array_keys($simple_permissions))
-                ->get()
-                ->pluck('guard', 'name')
-                ->all();
-        }
-
-        return new \Illuminate\Support\Collection($simple_permissions);
     }
 
     public function permissions(): BelongsToMany
