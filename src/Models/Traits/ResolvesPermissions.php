3b4be40e31e5f30f47c02b35999db25975b20f7a
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-11 18:23:43 +0200

    Add caching of model specific permissions with a cache time of 60 seconds.
    This needs additional refactoring to allow for configuration, but it will do for now.

--- src/Models/Traits/ResolvesPermissions.php
+++ src/Models/Traits/ResolvesPermissions.php
@@ -5,6 +5,7 @@ namespace Latus\Permissions\Models\Traits;
 
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Latus\Permissions\Models\Permission;
 
 trait ResolvesPermissions
@@ -14,33 +15,36 @@ trait ResolvesPermissions
 
     public function resolvePermissions(): Collection
     {
-        $simple_permissions = [];
+        return Cache::remember($this->getTable() . '_' . $this->getKey(), 60, function () {
+            $simple_permissions = [];
 
-        if (!empty($this->resolvable_relationship_methods)) {
-            foreach ($this->resolvable_relationship_methods as $relationship_method) {
-                $relationship = $this->$relationship_method()->get();
-                $simple_permissions += $relationship->resolvePermissions()->toArray();
+            if (!empty($this->resolvable_relationship_methods)) {
+                foreach ($this->resolvable_relationship_methods as $relationship_method) {
+                    $relationship = $this->$relationship_method()->get();
+                    $simple_permissions += $relationship->resolvePermissions()->toArray();
+                }
             }
-        }
 
-        $simple_permissions += $this->permissions()
-            ->where('name', 'not like', '%.*')
-            ->whereNotIn('name', array_keys($simple_permissions))
-            ->get()
-            ->pluck('guard', 'name')
-            ->all();
-
-        $wildcard_permissions = $this->permissions()->where('name', 'like', '%.*')->get()->pluck('guard', 'name')->all();
-
-        foreach ($wildcard_permissions as $wildcard_permission => $wildcard_permission_guard) {
-            $simple_permissions += Permission::where('name', 'like', substr($wildcard_permission, 0, -1) . '%')
+            $simple_permissions += $this->permissions()
                 ->where('name', 'not like', '%.*')
                 ->whereNotIn('name', array_keys($simple_permissions))
                 ->get()
                 ->pluck('guard', 'name')
                 ->all();
-        }
 
-        return new Collection($simple_permissions);
+            $wildcard_permissions = $this->permissions()->where('name', 'like', '%.*')->get()->pluck('guard', 'name')->all();
+
+            foreach ($wildcard_permissions as $wildcard_permission => $wildcard_permission_guard) {
+                $simple_permissions += Permission::where('name', 'like', substr($wildcard_permission, 0, -1) . '%')
+                    ->where('name', 'not like', '%.*')
+                    ->whereNotIn('name', array_keys($simple_permissions))
+                    ->get()
+                    ->pluck('guard', 'name')
+                    ->all();
+            }
+
+            return new Collection($simple_permissions);
+        });
+        
     }
 }
