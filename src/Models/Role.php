631099f93606f73ae32e91f6b38cd4151cbc8a8d
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-20 04:06:23 +0200

    Fix invalid import

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -5,7 +5,6 @@ namespace Latus\Permissions\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use App\Models\User;
 use Latus\Permissions\Models\Traits\Permissible;
 
 class Role extends Model
