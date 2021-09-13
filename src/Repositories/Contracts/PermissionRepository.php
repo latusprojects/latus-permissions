43660c865b8a68570891c07239b41a851596826b
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-13 22:14:00 +0200

    Add phpDoc comments to PermissionRepository.php contract and eloquent implementation methods

--- src/Repositories/Contracts/PermissionRepository.php
+++ src/Repositories/Contracts/PermissionRepository.php
@@ -11,17 +11,63 @@ use Latus\Repositories\Contracts\Repository;
 
 interface PermissionRepository extends Repository
 {
+    /**
+     * Attempts to delete a permission model
+     *
+     * @param Permission $permission
+     * @return mixed
+     */
     public function delete(Permission $permission);
 
+    /**
+     * Attempts to find a permission model by name
+     *
+     * @param string $name
+     * @return Permission|null
+     */
     public function findByName(string $name): Permission|null;
 
+    /**
+     * Grants a permission to a specific user
+     *
+     * @param Permission $permission
+     * @param Permissible $permissible
+     * @return mixed
+     */
     public function grantTo(Permission $permission, Permissible $permissible);
 
+    /**
+     * Revokes a permission from a specific user
+     *
+     * @param Permission $permission
+     * @param Permissible $permissible
+     * @return int
+     */
     public function revokeFrom(Permission $permission, Permissible $permissible): int;
 
+    /**
+     * Checks if a permission is granted to a specific user
+     *
+     * @param Permission $permission
+     * @param Permissible $permissible
+     * @param bool $resolvePermissions
+     * @return bool
+     */
     public function isGrantedTo(Permission $permission, Permissible $permissible, bool $resolvePermissions = true): bool;
 
+    /**
+     * Get roles that were granted this specific permission
+     *
+     * @param Permission $permission
+     * @return Collection
+     */
     public function getRoles(Permission $permission): Collection;
 
+    /**
+     * Get users that were granted this specific permission
+     *
+     * @param Permission $permission
+     * @return Collection
+     */
     public function getUsers(Permission $permission): Collection;
 }
