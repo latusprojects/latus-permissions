064814bf838aa70b35eee2f1b187c349e3638993
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-13 22:32:04 +0200

    Add phpDoc comments to methods

--- src/Models/Contracts/Permissible.php
+++ src/Models/Contracts/Permissible.php
@@ -9,11 +9,33 @@ use Illuminate\Support\Collection;
 
 interface Permissible
 {
+    /**
+     * Checks if this model has at least one of the specified permissions
+     *
+     * @param array $names
+     * @return bool
+     */
     public function hasOnePermission(array $names): bool;
 
+    /**
+     * Checks if this model has a specific permission
+     *
+     * @param string $name
+     * @return bool
+     */
     public function hasPermission(string $name): bool;
 
+    /**
+     * Gets permissions of this model, excluding those of parent and child models
+     *
+     * @return BelongsToMany
+     */
     public function permissions(): BelongsToMany;
 
+    /**
+     * Gets all permissions of this model, including those of parent and child models
+     *
+     * @return Collection
+     */
     public function resolvePermissions(): Collection;
 }
