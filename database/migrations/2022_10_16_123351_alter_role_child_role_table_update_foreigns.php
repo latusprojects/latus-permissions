0cce3a68291dda306d29e3894c916a38f17a462e
steelvirus <mlatussek@yahoo.de>
2022-10-17 10:04:15 +0200

    Add migrations for adding and updating foreign keys

---
+++ database/migrations/2022_10_16_123351_alter_role_child_role_table_update_foreigns.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('role_child_role', function (Blueprint $table) {
+            $table->foreign('role_id')->references('id')->on('roles')->cascadeOnDelete();
+            $table->foreign('child_role_id')->references('id')->on('roles')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
